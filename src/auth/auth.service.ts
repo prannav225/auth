d63cfb6035175934ef9ca4e59252ecef136aeec8
prannav225 <nagapranav.m@gmail.com>
2024-09-06 17:43:19 +0530

    encrypt, decrypt & hash the password

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,11 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
-import {JwtService} from '@nestjs/jwt';
+import { JwtService } from '@nestjs/jwt';
 import { Role } from './enums/roles.enums';
+import { createCipheriv, createDecipheriv, randomBytes, scrypt } from 'crypto';
+import { promisify } from 'util';
+import * as bcrypt from 'bcrypt';
+import { Permission } from './enums/permissions.enums';
 
 type authPayload = {
     username: string,
@@ -11,54 +15,79 @@ type authPayload = {
 type signInPayload = {
     userId: number,
     username: string,
-    role: Role[]
+    // role: Role[],
+    permission: Permission[]
 };
 type authResponse = {
     accessToken: string,
     userId: number,
-    username:string,
-    role: Role[]
+    username: string,
+    // role: Role[],
+    password: string,
+    permission: Permission[]
 };
 
 @Injectable()
 export class AuthService {
-    constructor(private usersService: UsersService, private jwtService: JwtService) {}
+    constructor(private usersService: UsersService, private jwtService: JwtService) { }
 
-    async authenticate(input: authPayload):Promise<authResponse> {
+    async authenticate(input: authPayload): Promise<authResponse> {
         const user = await this.validateUser(input);
+        const iv = randomBytes(16);
+        const key = (await promisify(scrypt)(input.password, 'salt', 32)) as Buffer;
+        const cipher = createCipheriv('aes-256-ctr', key, iv)
+        
 
-        if(!user){
+        const textToEncrypt = 'Nest';
+        const encryptedText = Buffer.concat([
+            cipher.update(textToEncrypt),
+            cipher.final(),
+        ]);
+
+        const decipher = createDecipheriv('aes-256-ctr', key, iv);
+        const decryptedText = Buffer.concat([
+            decipher.update(encryptedText),
+            decipher.final(),
+        ]);
+        const saltOrRounds = 10;
+        const hash = await bcrypt.hash(decryptedText, saltOrRounds)
+        if (!user) {
             throw new UnauthorizedException();
         }
 
-        return this.signIn(user)
+        return this.signIn(user, hash)
     }
 
     async validateUser(input: authPayload): Promise<signInPayload | null> {
         const user = await this.usersService.findUserByName(input.username);
-        
-        if(user && user.password === input.password) {
+
+
+        if (user && user.password === input.password) {
             return {
                 userId: user.userId,
                 username: user.username,
-                role: user.role
+                // role: user.role,
+                permission: user.permission
             };
         }
         return null;
     }
 
-    async signIn(user: signInPayload): Promise<authResponse> {
+    async signIn(user: signInPayload, hash: string): Promise<authResponse> {
         const tokenPayload = {
             sub: user.userId,
-            username:user.username,
-            role: user.role
+            username: user.username,
+            // role: user.role,
+            permission: user.permission
         };
         const accessToken = await this.jwtService.signAsync(tokenPayload);
         return {
             accessToken,
             username: user.username,
             userId: user.userId,
-            role: user.role
+            // role: user.role,
+            password: hash,
+            permission: user.permission
         }
     }
 }
